b308e79ec3d67f654ecfe833c3330d95d2c61a06
ZOWeston <zoneoutalternate@gmail.com>
2025-04-07 21:13:37 -0700

    HEHEHEHEHEHE

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ colorlist = ['red', 'green', 'pink', 'yellow' ]
 explodelist = [0.1, 0.0, 2.0, 0.0]
 # make the pie chart
 plot.pie(numlist, labels=namelist, autopct='%.2f%%', colors=colorlist,
-    	explode = explodelist, startangle = 90)
+    	explode = explodelist, startangle = 180)
 plot.axis('equal')
 plot.savefig('piechart.png')
 
