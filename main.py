6402bab6cbecaa5ac722b4f4a3e0eeb020b09bbd
ZOWeston <zoneoutalternate@gmail.com>
2025-04-07 21:16:50 -0700

    this be POPPIN

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ namelist = ['RED', 'GREEN', 'PINK', 'YELLOW']
 colorlist = ['red', 'green', 'pink', 'yellow' ]
 explodelist = [0.1, 0.0, 0.1, 0.0]
 # make the pie chart
-plot.pie(numlist, labels=namelist, autopct='%.2f%%', colors=colorlist,
+plot.pie(numlist, labels=namelist, autopct='%.4f%%', colors=colorlist,
     	explode = explodelist, startangle = 180)
 plot.axis('equal')
 plot.savefig('piechart.png')
