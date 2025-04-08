f38d7dacce6fe4ea62735e5816a7db877b3d48fa
ZOWeston <zoneoutalternate@gmail.com>
2025-04-07 21:25:04 -0700

    hmmm how can i make this about WEEZER?

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plot
 # set up your lists
 numlist = [2, 4, 10, 8]
 namelist = ['RED', 'GREEN', 'PINKERTON', 'BLUE']
-colorlist = ['red', 'green', 'black', 'yellow' ]
+colorlist = ['red', 'green', 'pink', 'blue' ]
 explodelist = [0.0, 0.0, 0.1, 0.1]
 # make the pie chart
 plot.pie(numlist, labels=namelist, autopct='%.4f%%', colors=colorlist,
