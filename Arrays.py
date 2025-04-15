bba1ad7e53534ff5d6bc125d8631d78e6999b3a1
ZOWeston <zoneoutalternate@gmail.com>
2025-04-14 20:11:16 -0700

    evil gang

---
+++ Arrays.py
@@ -0,0 +1,8 @@
+import numpy as np
+array = np.random.randint(1,10, size=(5,5), dtype=int)
+print(array)
+print(array[1,2])
+print(array.sum())
+print(np.mean(array, axis=1))
+print(np.max(array, axis=0))
+#:)
